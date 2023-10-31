292d1b45a4f972ec6f99f54f69cbbf01090e9f78
username <minhtuyenvp02@gmail.com>
2023-10-31 10:46:31 +0700

    initialize project

---
+++ src/main/java/com/group47/learning_web/LearningWebApplication.java
@@ -0,0 +1,13 @@
+package com.group47.learning_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group47/learning_web/LearningWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group47.learning_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

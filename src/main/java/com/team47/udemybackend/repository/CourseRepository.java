c5e7ba1cc69b86d0a190d32eff3b5f8db7568dad
minhtuyenvp02 <119645277+minhtuyenvp02@users.noreply.github.com>
2023-12-17 17:38:26 +0700

    add API findEnroledCourse and findEnrolledUser

--- src/main/java/com/team47/udemybackend/repository/CourseRepository.java
+++ src/main/java/com/team47/udemybackend/repository/CourseRepository.java
@@ -1,10 +1,12 @@
 package com.team47.udemybackend.repository;
 
 import com.team47.udemybackend.models.Course;
+import com.team47.udemybackend.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Integer> {
